99c7dfba2e34988ecf4065c144ff14fb3f601193
github username <github email address>
2019-09-25 18:52:28 -0700

    first problem

--- foo.js
+++ foo.js
@@ -1,5 +1,5 @@
 const readline = require('readline');
-const rl = readlin.createInterface({
+const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: false
@@ -9,4 +9,11 @@ const lines = [];
 rl.on('line', (line) =>lines.push(line));
 rl.on('close', (line) => compute(lines));
 
-function compute(lines){ console.log(lines)}
+function compute(lines){
+    let a = lines[0]
+    console.log("Knock knock.")
+    console.log("Who's there?")
+    console.log(lines[0])
+    console.log(a.substring(0, a.length-1), "who?")
+     console.log(lines[1])
+}
